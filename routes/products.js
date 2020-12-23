e3bd09551ab101a5a95495f10f032ba7df13a840
Sarah Markless <sarah.markless2@sky.uk>
2020-12-23 09:02:40 +0000

    backend

---
+++ routes/products.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+var fetch = require("node-fetch");
+
+const getData = async () => {
+  try {
+    const response = await fetch(
+      "https://openapi.etsy.com/v2/shops/DaliaBotanique/listings/active?&api_key=rxny6jmxqxkh4lsii4tzu26g&includes=MainImage,Images"
+    );
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+router.get("/", async (req, res, next) => {
+  let response = await getData();
+  res.send(response);
+});
+
+module.exports = router;
