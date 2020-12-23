aa0b8317e638642efbc72485bd23d8086214b075
Sarah Markless <sarah.markless2@sky.uk>
2020-12-23 12:40:19 +0000

    use axios instead of fetch

--- express/routes/products.js
+++ express/routes/products.js
@@ -1,18 +1,15 @@
 var express = require("express");
 var router = express.Router();
-var fetch = require("node-fetch").default;
-require("encoding");
+var axios = require("axios");
 
 const getData = async () => {
   console.log("GETDATA");
   try {
-    console.log("in try before fetch", response);
-    const response = await fetch(
+    const response = await axios.get(
       "https://openapi.etsy.com/v2/shops/DaliaBotanique/listings/active?&api_key=rxny6jmxqxkh4lsii4tzu26g&includes=MainImage,Images"
     );
     console.log("in try after", response);
-    const data = await response.json();
-    return data.results;
+    return response.data.results;
   } catch (error) {
     console.log("in catch");
     console.error(error);
